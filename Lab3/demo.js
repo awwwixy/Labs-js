2aed54bf04416912ca691572ee56e0140b2d1a1e
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-18 21:17:25 +0300

    Add LRU eviction with maxSize option to memoize

--- Lab3/demo.js
+++ Lab3/demo.js
@@ -10,3 +10,20 @@ const fastAdd = memoize(slowAdd);
 console.log(fastAdd(2, 3));
 console.log(fastAdd(2, 3));
 console.log(fastAdd(5, 7));
+
+console.log("-- Тест LRU --");
+
+const square = memoize(
+    (x) => {
+        console.log("рахую квадрат", x);
+        return x*x;
+    },
+    { maxSize: 2 }
+);
+
+square(1);
+square(2); 
+square(1); 
+square(3); 
+square(2); 
+square(1); 
