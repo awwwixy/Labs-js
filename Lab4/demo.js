38b6d6e33c0f50ff6d0c0998a95b2e5f343ab7e9
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-19 02:37:47 +0300

    add demo.js

---
+++ Lab4/demo.js
@@ -0,0 +1,21 @@
+import { PriorityQueue } from "./priorityQueue.js";
+
+const pq = new PriorityQueue();
+
+pq.enqueue("низький", 1);
+pq.enqueue("високий", 10);
+pq.enqueue("середній", 5);
+pq.enqueue("ще один високий", 10);
+
+console.log("-- peek (без видалення) --");
+console.log("highest:", pq.peek("highest"));
+console.log("lowest:", pq.peek("lowest"));
+console.log("oldest:", pq.peek(oldest));
+console.log("newest", pq.peek("newest"));
+
+console.log("-- dequeue (з видаленням) --");
+console.log(pq.dequeue("highest"));
+console.log(pq.dequeue("highest"));
+console.log(pq.dequeue("oldest"));
+console.log(pq.dequeue("newest"));
+console.log(pq.dequeue("highest"));
