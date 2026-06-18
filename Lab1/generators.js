63fbabef6c6b1f71dd283c04fb6ab19eb089eca7
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-18 14:54:06 +0300

    Lab1: timeout iterator with setInterval

--- Lab1/generators.js
+++ Lab1/generators.js
@@ -1,16 +1,28 @@
 function* dayGenerator(){
-    const days = ["Monday","Tuesday","Wednesday","Thurthday","Friday","Saturday","Sunday"];
+    const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
     let i = 0
 
-    while (true){
+    while (true) {
         yield days[i]
         i = (i + 1) % days.length;
        }
     }
 
-    const gen = dayGenerator();
-    console.log(gen.next().value);
-    console.log(gen.next().value);
-    console.log(gen.next().value);
-    console.log(gen.next().value);
-    console.log(gen.next().value);
+    function consumeWithTimeout(iterator, seconds) {
+        const start = Date.now();
+        let iteration = 0;
+
+        const timer = setInterval(() => {
+            if  (Date.now() - start >= seconds * 1000) {
+                clearInterval (timer);
+                return;
+            }
+
+            const value = iterator.next().value;
+            iteration++;
+            console.log(`Iteration ${iteration}: ${value}`);
+        }, 300);
+    }
+
+            const gen = dayGenerator();
+            consumeWithTimeout(gen, 3);
