c1b9581875513b6fe2235ff0dfc1a0e7a70b38d7
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-19 03:24:48 +0300

    add async array

--- Lab5/asyncArray.js
+++ Lab5/asyncArray.js
@@ -1,41 +1,36 @@
-export function asyncMapCallback(array, mapper,callback, signal) {
-    const result = [];
-    let index = 0;
-    comst timers = [];
+export function asyncMapCallback(array, mapper, callback, signal) {
+  const result = [];
+  let index = 0;
+  const timers = [];
 
-    function onAbort() {
-        timers.forEach(clearTimeout);
-        callback(new Error("Aborted"), null);
+  function onAbort() {
+    timers.forEach(clearTimeout);
+    callback(new Error("Aborted"), null);
+  }
+
+  if (signal) {
+    if (signal.aborted) {
+      callback(new Error("Aborted"), null);
+      return;
+    }
+    signal.addEventListener("abort", onAbort);
+  }
+
+  function processNext() {
+    if (index >= array.length) {
+      if (signal) signal.removeEventListener("abort", onAbort);
+      callback(null, result);
+      return;
     }
 
-    if (signal) {
-        if (signal.aborted) {
-            callback(new Error("Aborted"), null);
-        }
+    const timer = setTimeout(() => {
+      result.push(mapper(array[index]));
+      index++;
+      processNext();
+    }, 100);
 
-        if (signal) {
-            if (signal.aborted) {
-                callback(new Error("Aborted"), null);
-                return;
-            }
-            signal.addEventListener("abort", onAbort);
-        }
+    timers.push(timer);
+  }
 
-        function processNext() {
-            if (index >= array.lenght) {
-                if (signal.removeEventListener("abort", onAbort));
-                callback(null, result);
-                return;
-            }
-    
-            const timer = setTimeout(() => {
-             result.push(mapper(array[index]));
-             index++;
-             processNext();
-            }, 100);
-
-            timers.push(timer);
-        }
-
-        processNext(;)
-    }
+  processNext();
+}
