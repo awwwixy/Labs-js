71970b6c4f749a569fd47bcfe33bf68964a50374
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-19 05:05:15 +0300

    fix demo

--- Lab6/demo.js
+++ Lab6/demo.js
@@ -1,32 +1,31 @@
-import { dataStrem, processStream, faultyStream } from "./streamProcess.js";
+import { dataStream, processStream, faultyStream } from "./streamProcess.js";
 
 async function runNormal() {
-    console.log("-- Обробка потоку --");
-    const result = await processStream(dataStrem(5));
-    console.log("Результат:", result);
+  console.log("-- Обробка потоку --");
+  const result = await processStream(dataStream(5));
+  console.log("Результат:", result);
 }
 
 async function runBig() {
-    console.log("-- Великий потік --");
-    const result = awaite processStream(dataStrem(100));
-    console.log("Оброблено елементів:", result.count);
-    console.log("Сума:", result.sum);
+  console.log("-- Великий потік --");
+  const result = await processStream(dataStream(100));
+  console.log("Оброблено елементів:", result.count);
+  console.log("Сума:", result.sum);
 }
 
 async function runFaulty() {
-    console.log("Потік з помилкою");
-    try {
-        await processStream(faultyStream(10, 4));
-        console.log("Це не виведеться ");
-    } catch (err) {
-        console.log("Спіймати помилку:", err.massage);
-    }
+  console.log("-- Потік з помилкою --");
+  try {
+    await processStream(faultyStream(10, 4));
+    console.log("Це не виведеться ");
+  } catch (err) {
+    console.log("Спіймати помилку:", err.message);
+  }
 }
 
 async function main() {
-    await runNormal();
-    await runBig();
-    await runFaulty();
+  await runNormal();
+  await runBig();
+  await runFaulty();
 }
 main();
-    
