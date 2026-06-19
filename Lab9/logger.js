77f696939d606acfc6609faa471d76002264fa9a
Viktoriia Slyviuk <viktoriaslyviuk@gail.com>
2026-06-19 08:51:03 +0300

    fix logger decorator

--- Lab9/logger.js
+++ Lab9/logger.js
@@ -1,21 +1,20 @@
 export function log(fn, level = "INFO") {
-    return async function (...args) {
-        const timestemp = new Date() tiISOString();
+  return async function (...args) {
+    const timestemp = new Date().tiISOString();
 
-        try {
-            const result = await fn(...args);
+    try {
+      const result = await fn(...args);
 
-            if (level !== "ERROR") {
-                console.log(` [${timestemp}][${level}] виклик з аргументами:`, args);
-                console.log(`[${timestemp}][${level}] результат:`, result);
-            }
+      if (level !== "ERROR") {
+        console.log(` [${timestemp}][${level}] виклик з аргументами:`, args);
+        console.log(`[${timestemp}][${level}] результат:`, result);
+      }
 
-            return result;
-        } catch (err) {
-
-            console.log(`[${timestemp}] [ERROR] виклик з аргументами:`, args);
-            console.log(`[${timestemp}] [ERROR] помилка:`, err.message);
-            throw err;
-        }
-    };
+      return result;
+    } catch (err) {
+      console.log(`[${timestemp}] [ERROR] виклик з аргументами:`, args);
+      console.log(`[${timestemp}] [ERROR] помилка:`, err.message);
+      throw err;
+    }
+  };
 }
